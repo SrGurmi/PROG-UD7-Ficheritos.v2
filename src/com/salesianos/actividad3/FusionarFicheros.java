c8d4972dfc5995b59db90efa15a3bb7934fe195b
SrGurmi <94848171+SrGurmi@users.noreply.github.com>
2024-06-09 21:35:38 +0100

    Subir actividad 3, fusionar contenido

---
+++ src/com/salesianos/actividad3/FusionarFicheros.java
@@ -0,0 +1,34 @@
+package com.salesianos.actividad3;
+
+import com.salesianos.actividad3.utils.FileUtils;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FusionarFicheros {
+    public static void main(String[] args) {
+        String contenido1 = leerFichero("actividad1.txt");
+        String contenido2 = leerFichero("actividad2.txt");
+
+        String contenidoFusionado = "- Contenido del Fichero Uno: " + contenido1 + "\n"
+                + "- Contenido del Fichero Dos: " + contenido2 + "\n"
+                + "\n-- Firma: Proyecto PROG-UD7-Ficheritos";
+
+        FileUtils.saveTextToFile("actividad3.txt", contenidoFusionado);
+        System.out.println("Ficheros fusionados en actividad3.txt");
+    }
+
+    private static String leerFichero(String fileName) {
+        StringBuilder content = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                content.append(line).append("\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return content.toString();
+    }
+}
